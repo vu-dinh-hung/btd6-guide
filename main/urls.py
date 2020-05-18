a06fc9753d41c2ec5e769ddbb8f28a6b485118e4
Hung Vu <vudinhhung942k@gmail.com>
2020-05-18 11:17:28 +0700

    fix for incorrect urls (missing trailing slash in urlpatterns)

--- main/urls.py
+++ main/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('account/', views.account_user, name='account'),
-    path('<slug>', views.slug),
-    path('<path>/<slug>', views.slug),
+    path('<slug>/', views.slug),
+    path('<path>/<slug>/', views.slug),
 ]
