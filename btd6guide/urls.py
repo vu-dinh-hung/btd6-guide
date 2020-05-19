8b0b8395e33049e45a3571f985a0b9dccce08f7b
Hung Vu <vudinhhung942k@gmail.com>
2020-05-20 01:21:35 +0700

    fix for links not in main app not behaving correctly (admin, tinymce)

--- btd6guide/urls.py
+++ btd6guide/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('main.urls')),
-    path('tinymce/', include('tinymce.urls')),
     path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
+    path('', include('main.urls')),
 ]
